5eccf5f3c32029cf2cd05190c76747962d292488
trungtran286 <tt2861997@gmail.com>
2020-07-24 22:14:26 +0700

    up

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import TodoItems from './component/todoitems'
+// import TrafficLight from './component/TrafficLight'
 
 class App extends React.Component {
 	constructor() {
@@ -13,35 +14,48 @@ class App extends React.Component {
 		];
 	}
 
+	onItemClicked(event){
+		console.log("clicked",event);
+	}
+
 	render() {
+		// const { todoItems } = this.state;÷
 		// Cách dùng điều kiện if , else để xét điều kiện trả ra 
-		if(this.todoItems.length > 0){
-			return ( 
-				<div className = "App">
-					{
-						this.todoItems.map((items, index) => 
-						<TodoItems key={index} item={items} />
-						)
-					}
-				</div>
-			)
-		}
-		else {
-			return ( 
-				<div className = "App">
-					Nothing
-				</div>
-			)
-		}
-		// Cách dùng or hoặc and để xét điều kiện trả ra 
-		// return <div className = "App">
-		// 			{this.todoItems.length > 0 && this.todoItems.map((items, index) => 
+		// if(this.todoItems.length > 0){
+		// 	return ( 
+		// 		<div className = "App">
+		// 			{
+		// 				this.todoItems.map((items, index) => 
 		// 				<TodoItems key={index} item={items} />
 		// 				)
 		// 			}
-		// 			{this.todoItems.length === 0 && "Nothing"}
 		// 		</div>
-		
+		// 	)
+		// }
+		// else {
+		// 	return ( 
+		// 		<div className = "App">
+		// 			Nothing
+		// 		</div>
+		// 	)
+		// }
+		// Cách dùng or hoặc and để xét điều kiện trả ra 
+		return ( <div className = "App">
+					{this.todoItems.length > 0 && this.todoItems.map((items, index) => 
+						<TodoItems 
+							key={index} 
+							item={items} 
+							onClick={this.onItemClicked}/>
+						)
+					}
+					{this.todoItems.length === 0 && "Nothing"}
+				</div> )
+		// Ví dụ cụ thể về State 
+		// return ( <div className = "App">
+		// 		<TrafficLight />
+
+		// 	</div>
+		// )
 	}
 	
 }
--- src/component/todoitems.js
+++ src/component/todoitems.js
@@ -1,18 +1,24 @@
 import React from 'react';
-import './todoitems.css'
+import './todoitems.css';
+import className from 'classnames';
 
 export default class TodoItems extends React.Component {
+    // constructor(props){
+    //     super(props)
+    //     this.onItemClick = this.onItemClick.bind(this)// tại dòng này có nghĩa là nó sẽ trả về this trong hàm onItemClick đấy
+    // }
+    // onItemClick(){
+    //     console.log(this.props.item);// Ở đây chúng ta sẽ xảy ra lỗi nếu không có constructor phía bên trên vì nó không nhận được this nhận vào
+    // }
+
     render(){
         // console.log(this.props)
-        let { item } = this.props;
-        console.log(item)
-        let className = 'todoList';
-        if (item.isComplete){
-            className += ' todoList-done';
-        }
+        let { item , onClick} = this.props; // tạo item bằng this.props có nghĩa giá trị được truyền vào bên file App
+        // console.log(item)
+
         return(
-            <div className={className}>
-                <p>{this.props.item.title}</p>
+            <div onClick={onClick} className={className('todoList',{'todoList-done' : item.isComplete})}>
+                <p>{item.title}</p>
             </div>
         );
     }
