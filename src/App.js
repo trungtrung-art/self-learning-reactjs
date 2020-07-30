511254a0b2d1f2e1e1f3a32078e38503db955015
trungtran286 <tt2861997@gmail.com>
2020-07-30 11:40:58 +0700

    up

--- src/App.js
+++ src/App.js
@@ -1,19 +1,52 @@
 import React from 'react';
 import './App.css';
-import TrafficLight from './component/TrafficLight'
+import TodoItems from './component/todoitems'
+
 
 class App extends React.Component {
 	constructor() {
 		super();
+		this.todoItems=[
+			{ title : "Đi chơi gái", isComplete: true},
+			{ title : "Đi xét nghiệm", isComplete: true},
+			{ title : "Đi mua hòm", isComplete: true}
+		];
+	}
+
+	onItemClicked(items){
+		return (event) => {
+			// console.log(items);
+			const isComplete = items.isComplete;
+			const { todoItems } = this.state;
+			// tại dòng này có nghĩa là khi click vào bạn sẽ biết được nó thay đổi isCpmplete ở vị trí dòng nào 
+			const index = todoItems.indexOf(items);
+			this.setState({
+				todoItems: [
+					...todoItems.slice(0,index),
+					{
+						...items,
+						isComplete: !isComplete
+					},
+					...todoItems.slice (index + 1)
+				]
+			})
+		}
 		
 	}
 
 	render() {
+		// Cách dùng or hoặc and để xét điều kiện trả ra 
+		const { todoItems } = this.state;
 		return ( <div className = "App">
-				<TrafficLight />
-
-			</div>
-		)
+					{todoItems.length > 0 && todoItems.map((items, index) => 
+						<TodoItems 
+							key={index} 
+							item={items} 
+							onClick={this.onItemClicked(items)}/>
+						)
+					}
+					{todoItems.length === 0 && "Nothing"}
+				</div> )
 	}
 	
 }
