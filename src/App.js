150fb6e0c2c2c1bfd6ebc4349212085d1cda22fe
trungtran286 <tt2861997@gmail.com>
2020-08-21 00:31:59 +0700

    up

--- src/App.js
+++ src/App.js
@@ -1,3 +1,20 @@
 import React from 'react';
 import './App.css';
 
+default export class App extends Component {
+	render() {
+		return (
+		<div>
+			<h1>TASK</h1>
+			{tasks.map(item => (
+			<div key={item.id}>
+				Task name:
+				<strong onClick={() => { alert(item.content) }>
+				{item.content}
+				</strong>
+			</div>
+		))}
+		</div>
+	);
+	}
+}
