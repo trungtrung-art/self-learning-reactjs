aae1a4ee9036b942d2ab415a7187a7d052fc77f0
trungtran286 <tt2861997@gmail.com>
2020-08-21 00:05:50 +0700

    up

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends React.Component {
 	constructor() {
 		super();
 		this.state = {
+			newItems: "",
 			todoItems: [
 			{ title : "Đi chơi gái", isComplete: false},
 			{ title : "Đi xét nghiệm", isComplete: false},
@@ -17,23 +18,53 @@ class App extends React.Component {
 	}
 
 	onItemClicked(index){
+		console.log(index);
 		const arrList = [...this.state.todoItems]
 		arrList[index].isComplete = !arrList[index].isComplete
 		this.setState((prevState) => (
 			{
+				
 			...prevState,
 			todoItems: arrList
 		}))
 
 	}
 
+	onKeyUp(event){
+		let text = event.target.value
+		if(event.keyCode === 13){
+			console.log(text);
+			text = text.trim()
+			if (!text){
+				return;
+			}
+			this.setState({
+				newItems: "",
+				todoItems: [
+					...this.state.todoItems,
+					{ title: text, isComplete: false}
+				]
+			})
+		}
+	}
+
+	onChange(event){
+		this.setState({
+			newItems: event.target.value, //khi không enter thì nó sẽ lưu trữ text lên hàm newItems
+		});
+	}
+
 	render() {
 		// Cách dùng or hoặc and để xét điều kiện trả ra 
-		const { todoItems } = this.state;
+		const { todoItems , newItems} = this.state;
 		return ( <div className = "App">
 					<div className="Header">
 						<img src={checkall} width={32} height={32} />
-						<input />
+						<input 
+						placeholder="Nhập công việc cần làm vào đây ?" 
+						onKeyUp={this.onKeyUp.bind(this)} 
+						value = {newItems}
+						onChange={this.onChange.bind(this)}/>
 					</div>	
 					{todoItems.length > 0 && todoItems.map((items, index) => 
 						<TodoItems 
