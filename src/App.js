6091c8544ba58293fcf72880b109d69dccd53e4d
trungtran286 <tt2861997@gmail.com>
2020-09-07 23:41:39 +0700

    up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
 import './App.css';
-import Counter from './component/counter';
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 
+const Home = () => <h2>Home</h2>
+const About = () => <h2>About</h2>
 
-class App extends React.Component {
-	constructor() {
-		super();
-		
-	}
+function App (){
+	return(
+		<Router>
+			<div className="App">
+				<nav>
+					<ul>
+						<li>
+							<Link to="/">Home</Link>
+						</li>
+						<li>
+							<Link to="/about">About</Link>
+						</li>
+					</ul>
+				</nav>
+				<h1>đây là test về router</h1>
+				<Route path="/" exact component={Home}></Route>
+				<Route path="/about" exact component={About}></Route>
+			</div>
 
-	
-
-	render() {
-		console.log("App render");
-		return(
-			<Counter/>
-		)
-	}
-
-	componentDidUpdate(){
-		console.log("App update");
-	}
-	
+		</Router>
+	)
 }
 export default App;
